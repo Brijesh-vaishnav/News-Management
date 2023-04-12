477f205d37c89c8257d4cff448546037bb181ed0
brijesh <brijeshvaishnav508@gmail.com>
2023-04-12 14:43:03 +0530

    in the post states added

--- admin/add-post.php
+++ admin/add-post.php
@@ -1,55 +1,51 @@
-<?php 
+<?php
 session_start();
 include('includes/config.php');
 error_reporting(0);
-if(strlen($_SESSION['login'])==0)
-  { 
-header('location:index.php');
-}
-else{
+if (strlen($_SESSION['login']) == 0) {
+    header('location:index.php');
+} else {
 
-// For adding post  
-if(isset($_POST['submit']))
-{
-$posttitle=$_POST['posttitle'];
-$catid=$_POST['category'];
-$subcatid=$_POST['subcategory'];
-$postdetails=$_POST['postdescription'];
-$postedby=$_SESSION['login'];
-$arr = explode(" ",$posttitle);
-$url=implode("-",$arr);
-$imgfile=$_FILES["postimage"]["name"];
-// get the image extension
-$extension = substr($imgfile,strlen($imgfile)-4,strlen($imgfile));
-// allowed extensions
-$allowed_extensions = array(".jpg","jpeg",".png",".gif");
-// Validation for allowed extensions .in_array() function searches an array for a specific value.
-if(!in_array($extension,$allowed_extensions))
-{
-echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
-}
-else
-{
-//rename the image file
-$imgnewfile=md5($imgfile).$extension;
-// Code for move image into directory
-move_uploaded_file($_FILES["postimage"]["tmp_name"],"postimages/".$imgnewfile);
+    // For adding post  
+    if (isset($_POST['submit'])) {
+        $posttitle = $_POST['posttitle'];
+        $catid = $_POST['category'];
+        $subcatid = $_POST['subcategory'];
+        $postdetails = $_POST['postdescription'];
+        $postedby = $_SESSION['login'];
+        $post_state=$_POST["state"];
+        $arr = explode(" ", $posttitle);
+        $url = implode("-", $arr);
+        $imgfile = $_FILES["postimage"]["name"];
+        // get the image extension
+        $extension = substr($imgfile, strlen($imgfile) - 4, strlen($imgfile));
+        // allowed extensions
+        $allowed_extensions = array(".jpg", "jpeg", ".png", ".gif");
+        // Validation for allowed extensions .in_array() function searches an array for a specific value.
+        if (!in_array($extension, $allowed_extensions)) {
+            echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
+        } else {
+            //rename the image file
+            $imgnewfile = md5($imgfile) . $extension;
+            // Code for move image into directory
+            move_uploaded_file($_FILES["postimage"]["tmp_name"], "postimages/" . $imgnewfile);
 
-$status=1;
-$query=mysqli_query($con,"insert into tblposts(PostTitle,CategoryId,SubCategoryId,PostDetails,PostUrl,Is_Active,PostImage,postedBy) values('$posttitle','$catid','$subcatid','$postdetails','$url','$status','$imgnewfile','$postedby')");
-if($query)
-{
-$msg="Post successfully added ";
-}
-else{
-$error="Something went wrong . Please try again.";    
-} 
-
-}
-}
+            $is_active = 1;
+            $status=0;
+            if($_SESSION["login"]=="admin") 
+                $status=1;
+            $query = mysqli_query($con, "insert into tblposts(PostTitle,CategoryId,SubCategoryId,PostDetails,PostUrl,Is_Active,PostImage,postedBy,state,status) values('$posttitle','$catid','$subcatid','$postdetails','$url','$is_active','$imgnewfile','$postedby',' $post_state','$status')");
+            if ($query) {
+                $msg = "Post successfully added ";
+            } else {
+                $error = "Something went wrong . Please try again.";
+            }
+        }
+    }
 ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -79,20 +75,20 @@ $error="Something went wrong . Please try again.";
         <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
         <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
         <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
-		<link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
+        <link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
         <script src="assets/js/modernizr.min.js"></script>
- <script>
-function getSubCat(val) {
-  $.ajax({
-  type: "POST",
-  url: "get_subcategory.php",
-  data:'catid='+val,
-  success: function(data){
-    $("#subcategory").html(data);
-  }
-  });
-  }
-  </script>
+        <script>
+            function getSubCat(val) {
+                $.ajax({
+                    type: "POST",
+                    url: "get_subcategory.php",
+                    data: 'catid=' + val,
+                    success: function(data) {
+                        $("#subcategory").html(data);
+                    }
+                });
+            }
+        </script>
     </head>
 
 
@@ -102,9 +98,9 @@ function getSubCat(val) {
         <div id="wrapper">
 
             <!-- Top Bar Start -->
-           <?php include('includes/topheader.php');?>
+            <?php include('includes/topheader.php'); ?>
             <!-- ========== Left Sidebar Start ========== -->
-             <?php include('includes/leftsidebar.php');?>
+            <?php include('includes/leftsidebar.php'); ?>
             <!-- Left Sidebar End -->
 
 
@@ -119,8 +115,8 @@ function getSubCat(val) {
 
 
                         <div class="row">
-							<div class="col-xs-12">
-								<div class="page-title-box">
+                            <div class="col-xs-12">
+                                <div class="page-title-box">
                                     <h4 class="page-title">Add Post </h4>
                                     <ol class="breadcrumb p-0 m-0">
                                         <li>
@@ -135,88 +131,126 @@ function getSubCat(val) {
                                     </ol>
                                     <div class="clearfix"></div>
                                 </div>
-							</div>
-						</div>
+                            </div>
+                        </div>
                         <!-- end row -->
 
-<div class="row">
-<div class="col-sm-6">  
-<!---Success Message--->  
-<?php if($msg){ ?>
-<div class="alert alert-success" role="alert">
-<strong>Well done!</strong> <?php echo htmlentities($msg);?>
-</div>
-<?php } ?>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <!---Success Message--->
+                                <?php if ($msg) { ?>
+                                    <div class="alert alert-success" role="alert">
+                                        <strong>Well done!</strong> <?php echo htmlentities($msg); ?>
+                                    </div>
+                                <?php } ?>
 
-<!---Error Message--->
-<?php if($error){ ?>
-<div class="alert alert-danger" role="alert">
-<strong>Oh snap!</strong> <?php echo htmlentities($error);?></div>
-<?php } ?>
+                                <!---Error Message--->
+                                <?php if ($error) { ?>
+                                    <div class="alert alert-danger" role="alert">
+                                        <strong>Oh snap!</strong> <?php echo htmlentities($error); ?>
+                                    </div>
+                                <?php } ?>
 
 
-</div>
-</div>
+                            </div>
+                        </div>
 
 
                         <div class="row">
                             <div class="col-md-10 col-md-offset-1">
                                 <div class="p-6">
                                     <div class="">
-<form name="addpost" method="post" enctype="multipart/form-data">
- <div class="form-group m-b-20">
-<label for="exampleInputEmail1">Post Title</label>
-<input type="text" class="form-control" id="posttitle" name="posttitle" placeholder="Enter title" required>
-</div>
+                                        <form name="addpost" method="post" enctype="multipart/form-data">
+                                            <div class="form-group m-b-20">
+                                                <label for="exampleInputEmail1">Post Title</label>
+                                                <input type="text" class="form-control" id="posttitle" name="posttitle" placeholder="Enter title" required>
+                                            </div>
 
 
 
-<div class="form-group m-b-20">
-<label for="exampleInputEmail1">Category</label>
-<select class="form-control" name="category" id="category" onChange="getSubCat(this.value);" required>
-<option value="">Select Category </option>
-<?php
-// Feching active categories
-$ret=mysqli_query($con,"select id,CategoryName from  tblcategory where Is_Active=1");
-while($result=mysqli_fetch_array($ret))
-{    
-?>
-<option value="<?php echo htmlentities($result['id']);?>"><?php echo htmlentities($result['CategoryName']);?></option>
-<?php } ?>
+                                            <div class="form-group m-b-20">
+                                                <label for="exampleInputEmail1">Category</label>
+                                                <select class="form-control" name="category" id="category" onChange="getSubCat(this.value);" required>
+                                                    <option value="">Select Category </option>
+                                                    <?php
+                                                    // Feching active categories
+                                                    $ret = mysqli_query($con, "select id,CategoryName from  tblcategory where Is_Active=1");
+                                                    while ($result = mysqli_fetch_array($ret)) {
+                                                    ?>
+                                                        <option value="<?php echo htmlentities($result['id']); ?>"><?php echo htmlentities($result['CategoryName']); ?></option>
+                                                    <?php } ?>
 
-</select> 
-</div>
-    
-<div class="form-group m-b-20">
-<label for="exampleInputEmail1">Sub Category</label>
-<select class="form-control" name="subcategory" id="subcategory" required>
+                                                </select>
+                                            </div>
 
-</select> 
-</div>
-         
+                                            <div class="form-group m-b-20">
+                                                <label for="exampleInputEmail1">Sub Category</label>
+                                                <select class="form-control" name="subcategory" id="subcategory" required>
 
-<div class="row">
-<div class="col-sm-12">
- <div class="card-box">
-<h4 class="m-b-30 m-t-0 header-title"><b>Post Details</b></h4>
-<textarea class="summernote" name="postdescription" required></textarea>
-</div>
-</div>
-</div>
+                                                </select>
+                                            </div>
 
 
-<div class="row">
-<div class="col-sm-12">
- <div class="card-box">
-<h4 class="m-b-30 m-t-0 header-title"><b>Feature Image</b></h4>
-<input type="file" class="form-control" id="postimage" name="postimage"  required>
-</div>
-</div>
-</div>
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <div class="card-box">
+                                                        <h4 class="m-b-30 m-t-0 header-title"><b>Post Details</b></h4>
+                                                        <textarea class="summernote" name="postdescription" required></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
 
 
-<button type="submit" name="submit" class="btn btn-success waves-effect waves-light">Save and Post</button>
- <button type="button" class="btn btn-danger waves-effect waves-light">Discard</button>
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <div class="card-box">
+                                                        <h4 class="m-b-30 m-t-0 header-title"><b>Feature Image</b></h4>
+                                                        <input type="file" class="form-control" id="postimage" name="postimage" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row m-b-30">
+                                                <div class="col-sm-12">
+                                                    <div class="card-box">
+                                                        <h4 class="m-b-30 m-t-0 header-title"><b>Select State</b></h4>
+                                                        <select name="state">
+                                                            <option value="0">Andhra Pradesh</option>
+                                                            <option value="1">Arunachal Pradesh</option>
+                                                            <option value="2">Assam</option>
+                                                            <option value="3">Bihar</option>
+                                                            <option value="4">Chhattisgarh</option>
+                                                            <option value="5">Goa</option>
+                                                            <option value="6">Gujarat</option>
+                                                            <option value="7">Haryana</option>
+                                                            <option value="8">Himachal Pradesh</option>
+                                                            <option value="9">Jharkhand</option>
+                                                            <option value="10">Karnataka</option>
+                                                            <option value="11">Kerala</option>
+                                                            <option value="12">Madhya Pradesh</option>
+                                                            <option value="13">Maharashtra</option>
+                                                            <option value="14">Manipur</option>
+                                                            <option value="15">Meghalaya</option>
+                                                            <option value="16">Mizoram</option>
+                                                            <option value="17">Nagaland</option>
+                                                            <option value="18">Odisha</option>
+                                                            <option value="19">Punjab</option>
+                                                            <option value="20">Rajasthan</option>
+                                                            <option value="21">Sikkim</option>
+                                                            <option value="22">Tamil Nadu</option>
+                                                            <option value="23">Telangana</option>
+                                                            <option value="24">Tripura</option>
+                                                            <option value="25">Uttar Pradesh</option>
+                                                            <option value="26">Uttarakhand</option>
+                                                            <option value="27">West Bengal</option>
+                                                        </select>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                            <button type="submit" name="submit" class="btn btn-success waves-effect waves-light">Save and Post</button>
+                                            <button type="button" class="btn btn-danger waves-effect waves-light">Discard</button>
                                         </form>
                                     </div>
                                 </div> <!-- end p-20 -->
@@ -230,7 +264,7 @@ while($result=mysqli_fetch_array($ret))
 
                 </div> <!-- content -->
 
-           <?php include('includes/footer.php');?>
+                <?php include('includes/footer.php'); ?>
 
             </div>
 
@@ -275,14 +309,13 @@ while($result=mysqli_fetch_array($ret))
         <script src="assets/js/jquery.app.js"></script>
 
         <script>
-
-            jQuery(document).ready(function(){
+            jQuery(document).ready(function() {
 
                 $('.summernote').summernote({
-                    height: 240,                 // set editor height
-                    minHeight: null,             // set minimum height of editor
-                    maxHeight: null,             // set maximum height of editor
-                    focus: false                 // set focus to editable area after initializing summernote
+                    height: 240, // set editor height
+                    minHeight: null, // set minimum height of editor
+                    maxHeight: null, // set maximum height of editor
+                    focus: false // set focus to editable area after initializing summernote
                 });
                 // Select2
                 $(".select2").select2();
@@ -292,14 +325,15 @@ while($result=mysqli_fetch_array($ret))
                 });
             });
         </script>
-  <script src="../plugins/switchery/switchery.min.js"></script>
+        <script src="../plugins/switchery/switchery.min.js"></script>
 
         <!--Summernote js-->
         <script src="../plugins/summernote/summernote.min.js"></script>
 
-    
+
 
 
     </body>
-</html>
+
+    </html>
 <?php } ?>
